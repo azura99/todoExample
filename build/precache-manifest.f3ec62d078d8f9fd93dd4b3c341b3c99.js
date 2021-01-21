21722d18ba3067919e4714c652fa06c75992084a
ali <aliasimgg@gmail.com>
2021-01-21 15:18:07 -0500

    added build bc

---
+++ build/precache-manifest.f3ec62d078d8f9fd93dd4b3c341b3c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7cee115260a753f8253c35e1067c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ee28a50f850e371de9e",
+    "url": "/static/css/main.1e0d6d27.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee28a50f850e371de9e",
+    "url": "/static/js/main.92e18672.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
